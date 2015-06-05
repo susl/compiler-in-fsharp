acc692449e7c6bd5ba80c563ace455571ab75b66
Dmytro Suvorov <suslamo@gmail.com>
2015-06-05 16:57:54 +0300

    add build config for releasing slides

--- 1-fsharp-basics-and-intro-to-pls/slides/build.fsx
+++ 1-fsharp-basics-and-intro-to-pls/slides/build.fsx
@@ -9,10 +9,10 @@
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted
-let gitOwner = "myGitUser"
+let gitOwner = "susl"
 let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
-let gitProjectName = "MyProject"
+let gitProjectName = "compiler-in-fsharp"
 
 open FsReveal
 open Fake
